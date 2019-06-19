b7c8a3e9bb87a689448d01085e04dc794c2f2ada
galtnie <hereby@protonmail.com>
2019-06-19 10:23:37 +0300

    refactored list file

--- src/list.js
+++ src/list.js
@@ -4,14 +4,7 @@ import CustomItem from "./customItem";
 import { setID } from "./functions";
 
 const List = () => {
-  const [name, setName] = useState("");
   const [items, setItems] = useState([]);
-
-  const checkName = () => {
-    name.length > 0
-      ? addNewItem(name)
-      : alert("No item will be added without a name.");
-  };
 
   const addNewItem = name => {
     const newItem = {
@@ -20,7 +13,6 @@ const List = () => {
       sublist: false
     };
 
-    setName("");
     setItems(prev => [newItem, ...prev]);
   };
 
@@ -47,78 +39,3 @@ const List = () => {
 };
 
 export default List;
-
-// const addNewItem = name => {
-//   const id = setID();
-//   setItems(prev => [
-//     {
-//       id,
-//       value: (
-//         <CustomLi
-//           name={name}
-//           id={id}
-//           up={defineUpButtonDisplayValue}
-//           down={defineDownButtonDisplayValue}
-//           key={id}
-//           index={0}
-//           items={prev.length + 1}
-//           getIndex={getIndex}
-//           getItemsLength={getItemsLength}
-//         />
-//       )
-//     },
-//     ...prev
-//   ]);
-// };
-
-// const defineUpButtonDisplayValue = id => {
-//   const index = items.findIndex(i => i.id === id);
-//   return index > 0 ? "inline" : "none";
-// };
-
-// const defineDownButtonDisplayValue = id => {
-//   const index = items.findIndex(i => i.id === id);
-//   return index < items.length - 2 ? "inline" : "none";
-// };
-
-// useEffect(() => {
-//   let index = 0;
-
-//   items.forEach(i => {
-//     console.log(i.value);
-//     i.value.index = index++;
-//   });
-// }, [items]);
-
-// const getIndex = id => {
-//   return items.findIndex(i => i.id === id);
-// };
-
-// const getItemsLength = () => {
-//   return items.length;
-// };
-
-// <li>
-//           {i.name}
-//           {up && <button>up</button>}
-//           {down && <button>down</button>}
-//           {<button>remove</button>}
-//           {i.sublist ? (
-//             <button
-//               onClick={() => {
-//                 i.sublist = false;
-//               }}
-//             >
-//               remove sublist
-//             </button>
-//           ) : (
-//             <button
-//               onClick={() => {
-//                 i.sublist = true;
-//               }}
-//             >
-//               {" "}
-//               add sublist{" "}
-//             </button>
-//           )}
-//         </li>
